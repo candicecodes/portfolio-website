e671e420a3ad7329ef5680de0d060f565a7ab089
Candice Mayes <ca.mayes239@gmail.com>
2017-12-11 12:40:42 -0500

    nav functionality

---
+++ dev/scripts/main.js
@@ -0,0 +1,29 @@
+$(function() {
+    var scroll = new SmoothScroll('a[href*="#about"]', {
+        speed: 1000,
+        easing: 'easeInOutCubic',
+        offset: 90
+    
+    });
+
+    var scroll = new SmoothScroll('a[href*="#portfolio"]', {
+        speed: 1000,
+        easing: 'easeInOutCubic',
+        offset: 90
+
+    });
+
+    var scroll = new SmoothScroll('a[href*="#skills"]', {
+        speed: 1000,
+        easing: 'easeInOutCubic',
+        offset: 90
+
+    });
+
+    var scroll = new SmoothScroll('a[href*="#contact"]', {
+        speed: 1000,
+        easing: 'easeInOutCubic',
+        offset: 90
+
+    });
+});
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,31 @@
-"use strict";
+'use strict';
+
+$(function () {
+    var scroll = new SmoothScroll('a[href*="#about"]', {
+        speed: 1000,
+        easing: 'easeInOutCubic',
+        offset: 90
+
+    });
+
+    var scroll = new SmoothScroll('a[href*="#portfolio"]', {
+        speed: 1000,
+        easing: 'easeInOutCubic',
+        offset: 90
+
+    });
+
+    var scroll = new SmoothScroll('a[href*="#skills"]', {
+        speed: 1000,
+        easing: 'easeInOutCubic',
+        offset: 90
+
+    });
+
+    var scroll = new SmoothScroll('a[href*="#contact"]', {
+        speed: 1000,
+        easing: 'easeInOutCubic',
+        offset: 90
+
+    });
+});
