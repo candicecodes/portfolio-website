abcf98e73bf744541d2decaec1ba4e587871edfb
Candice Mayes <ca.mayes239@gmail.com>
2017-12-11 15:42:44 -0500

    dynamic header

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -22,4 +22,27 @@ $(function() {
         easing: 'easeInOutCubic',
         offset: 90
     });
+
+    const homeSection = $('#home').offset().top;
+    const aboutSection = $('#about').offset().top;
+    const portfolioSection = $('#portfolio').offset().top;
+    const skillsSection = $('#skills').offset().top;
+    const contactSection = $('#contact').offset().top;
+
+    $(document).scroll(function() {
+        let scrollPosition = $(document).scrollTop();
+        if(scrollPosition >= homeSection && scrollPosition < aboutSection) {
+            $('#sectionName').text(' ');
+        } else if (scrollPosition >= aboutSection && scrollPosition < portfolioSection) {
+            $('#sectionName').text('About');
+        } else if (scrollPosition >= portfolioSection && scrollPosition < skillsSection) {
+            $('#sectionName').text('Portfolio');
+        } else if (scrollPosition >= skillsSection && scrollPosition < contactSection) {
+            $('#sectionName').text('Skills');
+         
+        } else if (scrollPosition >= contactSection) {
+            $('#sectionName').text('Contact');
+        }
+
+    });
 });
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -24,4 +24,25 @@ $(function () {
         easing: 'easeInOutCubic',
         offset: 90
     });
+
+    var homeSection = $('#home').offset().top;
+    var aboutSection = $('#about').offset().top;
+    var portfolioSection = $('#portfolio').offset().top;
+    var skillsSection = $('#skills').offset().top;
+    var contactSection = $('#contact').offset().top;
+
+    $(document).scroll(function () {
+        var scrollPosition = $(document).scrollTop();
+        if (scrollPosition >= homeSection && scrollPosition < aboutSection) {
+            $('#sectionName').text(' ');
+        } else if (scrollPosition >= aboutSection && scrollPosition < portfolioSection) {
+            $('#sectionName').text('About');
+        } else if (scrollPosition >= portfolioSection && scrollPosition < skillsSection) {
+            $('#sectionName').text('Portfolio');
+        } else if (scrollPosition >= skillsSection && scrollPosition < contactSection) {
+            $('#sectionName').text('Skills');
+        } else if (scrollPosition >= contactSection) {
+            $('#sectionName').text('Contact');
+        }
+    });
 });
