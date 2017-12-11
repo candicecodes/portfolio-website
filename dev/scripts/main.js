b69a0b9ed9ae5f1cd3d5becd6358d0a68916f72d
Candice Mayes <ca.mayes239@gmail.com>
2017-12-11 17:21:35 -0500

    dynamic header

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -23,17 +23,34 @@ $(function() {
         offset: 90
     });
 
-    const homeSection = $('#home').offset().top;
-    const aboutSection = $('#about').offset().top;
-    const portfolioSection = $('#portfolio').offset().top;
-    const skillsSection = $('#skills').offset().top;
-    const contactSection = $('#contact').offset().top;
+    // const homeSection = $('#home').offset().top;
+    // const aboutSection = $('#about').offset().top - 300;
+    // const portfolioSection = $('#portfolio').offset().top - 300;
+    // const skillsSection = $('#skills').offset().top - 300;
+    // const contactSection = $('#contact').offset().top - 300;
+
+    // console.log(aboutSection);
+    // console.log(portfolioSection);
+    // console.log(skillsSection);
+    // console.log(contactSection);
+
+    let homeSectionTop = $('#home').offset().top;
+    let aboutSectionTop = $('#about').offset().top;
+    let portfolioSectionTop = $('#portfolio').offset().top;
+    let skillsSectionTop = $('#skills').offset().top;
+    let contactSectionTop = $('#contact').offset().top;
+
+    let homeSectionBottom = $('#home').offset().top + $('#home').outerHeight(true);
+    let aboutSectionBottom = $('#about').offset().top + $('#about').outerHeight(true);
+    let portfolioSectionBottom = $('#portfolio').offset().top + $('#portfolio').outerHeight(true);
+    let skillsSectionBottom = $('#skills').offset().top + $('#skills').outerHeight(true);
+    let contactSectionBottom = $('#contact').offset().top + $('#contact').outerHeight(true);
 
     $(document).scroll(function() {
         let scrollPosition = $(document).scrollTop();
         if(scrollPosition >= homeSection && scrollPosition < aboutSection) {
             $('#sectionName').text(' ');
-        } else if (scrollPosition >= aboutSection && scrollPosition < portfolioSection) {
+        } else if (scrollPosition >= aboutSection && scrollPosition <= portfolioSection) {
             $('#sectionName').text('About');
         } else if (scrollPosition >= portfolioSection && scrollPosition < skillsSection) {
             $('#sectionName').text('Portfolio');
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -25,17 +25,34 @@ $(function () {
         offset: 90
     });
 
-    var homeSection = $('#home').offset().top;
-    var aboutSection = $('#about').offset().top;
-    var portfolioSection = $('#portfolio').offset().top;
-    var skillsSection = $('#skills').offset().top;
-    var contactSection = $('#contact').offset().top;
+    // const homeSection = $('#home').offset().top;
+    // const aboutSection = $('#about').offset().top - 300;
+    // const portfolioSection = $('#portfolio').offset().top - 300;
+    // const skillsSection = $('#skills').offset().top - 300;
+    // const contactSection = $('#contact').offset().top - 300;
+
+    // console.log(aboutSection);
+    // console.log(portfolioSection);
+    // console.log(skillsSection);
+    // console.log(contactSection);
+
+    var homeSectionTop = $('#home').offset().top;
+    var aboutSectionTop = $('#about').offset().top;
+    var portfolioSectionTop = $('#portfolio').offset().top;
+    var skillsSectionTop = $('#skills').offset().top;
+    var contactSectionTop = $('#contact').offset().top;
+
+    var homeSectionBottom = $('#home').offset().top + $('#home').outerHeight(true);
+    var aboutSectionBottom = $('#about').offset().top + $('#about').outerHeight(true);
+    var portfolioSectionBottom = $('#portfolio').offset().top + $('#portfolio').outerHeight(true);
+    var skillsSectionBottom = $('#skills').offset().top + $('#skills').outerHeight(true);
+    var contactSectionBottom = $('#contact').offset().top + $('#contact').outerHeight(true);
 
     $(document).scroll(function () {
         var scrollPosition = $(document).scrollTop();
         if (scrollPosition >= homeSection && scrollPosition < aboutSection) {
             $('#sectionName').text(' ');
-        } else if (scrollPosition >= aboutSection && scrollPosition < portfolioSection) {
+        } else if (scrollPosition >= aboutSection && scrollPosition <= portfolioSection) {
             $('#sectionName').text('About');
         } else if (scrollPosition >= portfolioSection && scrollPosition < skillsSection) {
             $('#sectionName').text('Portfolio');
