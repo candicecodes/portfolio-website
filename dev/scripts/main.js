750b881dfe6c11b69326954ed665e8b27a390f14
Candice Mayes <ca.mayes239@gmail.com>
2017-12-11 17:55:13 -0500

    more jquery

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -23,28 +23,37 @@ $(function() {
         offset: 90
     });
 
-    // const homeSection = $('#home').offset().top;
-    // const aboutSection = $('#about').offset().top - 300;
-    // const portfolioSection = $('#portfolio').offset().top - 300;
-    // const skillsSection = $('#skills').offset().top - 300;
-    // const contactSection = $('#contact').offset().top - 300;
+    const homeSection = $('#home').offset().top;
+    const aboutSection = $('#about').offset().top - 95;
+    const portfolioSection = $('#portfolio').offset().top - 95;
+    const skillsSection = $('#skills').offset().top - 95;
+    const contactSection = $('#contact').offset().top - 300;
 
-    // console.log(aboutSection);
-    // console.log(portfolioSection);
-    // console.log(skillsSection);
-    // console.log(contactSection);
+    console.log(aboutSection);
+    console.log(portfolioSection);
+    console.log(skillsSection);
+    console.log(contactSection);
 
-    let homeSectionTop = $('#home').offset().top;
-    let aboutSectionTop = $('#about').offset().top;
-    let portfolioSectionTop = $('#portfolio').offset().top;
-    let skillsSectionTop = $('#skills').offset().top;
-    let contactSectionTop = $('#contact').offset().top;
+    // let homeSectionTop = $('#home').offset().top;
+    // let aboutSectionTop = $('#about').offset().top;
+    // let portfolioSectionTop = $('#portfolio').offset().top;
+    // let skillsSectionTop = $('#skills').offset().top;
+    // let contactSectionTop = $('#contact').offset().top;
 
-    let homeSectionBottom = $('#home').offset().top + $('#home').outerHeight(true);
-    let aboutSectionBottom = $('#about').offset().top + $('#about').outerHeight(true);
-    let portfolioSectionBottom = $('#portfolio').offset().top + $('#portfolio').outerHeight(true);
-    let skillsSectionBottom = $('#skills').offset().top + $('#skills').outerHeight(true);
-    let contactSectionBottom = $('#contact').offset().top + $('#contact').outerHeight(true);
+    // let homeSectionBottom = $('#home').offset().top + $('#home').outerHeight(true);
+    // let aboutSectionBottom = $('#about').offset().top + $('#about').outerHeight(true);
+    // let portfolioSectionBottom = $('#portfolio').offset().top + $('#portfolio').outerHeight(true);
+    // let skillsSectionBottom = $('#skills').offset().top + $('#skills').outerHeight(true);
+    // let contactSectionBottom = $('#contact').offset().top + $('#contact').outerHeight(true);
+
+    // $(document).scroll(function() {
+    //     let scrollPosition = $(document).scrollTop();
+    //     if(scrollPosition > homeSectionTop && scrollPosition < homeSectionBottom) {
+    //         $('#sectionName').text(' ');
+    //     } else if(scrollPosition > aboutSectionTop && scrollPosition < aboutSectionBottom) {
+    //         $('#sectionName').text('About');
+    //     }
+    // });
 
     $(document).scroll(function() {
         let scrollPosition = $(document).scrollTop();
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -25,28 +25,37 @@ $(function () {
         offset: 90
     });
 
-    // const homeSection = $('#home').offset().top;
-    // const aboutSection = $('#about').offset().top - 300;
-    // const portfolioSection = $('#portfolio').offset().top - 300;
-    // const skillsSection = $('#skills').offset().top - 300;
-    // const contactSection = $('#contact').offset().top - 300;
+    var homeSection = $('#home').offset().top;
+    var aboutSection = $('#about').offset().top - 95;
+    var portfolioSection = $('#portfolio').offset().top - 95;
+    var skillsSection = $('#skills').offset().top - 95;
+    var contactSection = $('#contact').offset().top - 300;
 
-    // console.log(aboutSection);
-    // console.log(portfolioSection);
-    // console.log(skillsSection);
-    // console.log(contactSection);
+    console.log(aboutSection);
+    console.log(portfolioSection);
+    console.log(skillsSection);
+    console.log(contactSection);
 
-    var homeSectionTop = $('#home').offset().top;
-    var aboutSectionTop = $('#about').offset().top;
-    var portfolioSectionTop = $('#portfolio').offset().top;
-    var skillsSectionTop = $('#skills').offset().top;
-    var contactSectionTop = $('#contact').offset().top;
+    // let homeSectionTop = $('#home').offset().top;
+    // let aboutSectionTop = $('#about').offset().top;
+    // let portfolioSectionTop = $('#portfolio').offset().top;
+    // let skillsSectionTop = $('#skills').offset().top;
+    // let contactSectionTop = $('#contact').offset().top;
 
-    var homeSectionBottom = $('#home').offset().top + $('#home').outerHeight(true);
-    var aboutSectionBottom = $('#about').offset().top + $('#about').outerHeight(true);
-    var portfolioSectionBottom = $('#portfolio').offset().top + $('#portfolio').outerHeight(true);
-    var skillsSectionBottom = $('#skills').offset().top + $('#skills').outerHeight(true);
-    var contactSectionBottom = $('#contact').offset().top + $('#contact').outerHeight(true);
+    // let homeSectionBottom = $('#home').offset().top + $('#home').outerHeight(true);
+    // let aboutSectionBottom = $('#about').offset().top + $('#about').outerHeight(true);
+    // let portfolioSectionBottom = $('#portfolio').offset().top + $('#portfolio').outerHeight(true);
+    // let skillsSectionBottom = $('#skills').offset().top + $('#skills').outerHeight(true);
+    // let contactSectionBottom = $('#contact').offset().top + $('#contact').outerHeight(true);
+
+    // $(document).scroll(function() {
+    //     let scrollPosition = $(document).scrollTop();
+    //     if(scrollPosition > homeSectionTop && scrollPosition < homeSectionBottom) {
+    //         $('#sectionName').text(' ');
+    //     } else if(scrollPosition > aboutSectionTop && scrollPosition < aboutSectionBottom) {
+    //         $('#sectionName').text('About');
+    //     }
+    // });
 
     $(document).scroll(function () {
         var scrollPosition = $(document).scrollTop();
